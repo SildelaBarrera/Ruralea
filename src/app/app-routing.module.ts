9ca70a29739cf030494b1ea4cb7c548e63674251
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-04-30 14:02:14 +0200

    commit card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MisEventosComponent } from './pages/mis-eventos/mis-eventos.component';
 
 
 const routes: Routes = [
   {path:"landing", component: LandingPageComponent},
   {path:"registro", component:RegistroComponent},
-  {path:"login", component: LoginComponent}
+  {path:"login", component: LoginComponent},
+  {path:"misEventos", component: MisEventosComponent}
 ];
 
 @NgModule({

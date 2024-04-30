3aacf06505d710920741a193728b77acfc53a7f3
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-04-30 16:01:25 +0200

    mis eventos

--- src/app/shared/evento-service.service.ts
+++ src/app/shared/evento-service.service.ts
@@ -11,13 +11,13 @@ export class EventoServiceService {
   constructor() {
     this.eventos = [
       new Evento("TALLER DE CERÁMICA", "Artesanía", "15 de julio de 2024", "Belmonte, Cuenca", 10, 15, "Demostración de la elaboración de productos tradicionales" + 
-      "y creación de una pieza propia para llevarse a casa. Duración aproximada de 3h.", "https://images.pexels.com/photos/9736884/pexels-photo-9736884.jpeg"),
-      new Evento("PASTOR POR UN DÍA", "Ganadería", "20 de mayo de 2024", "Arroyo de la Luz, Cáceres", 10, 18, "Demostración de la elaboración de productos tradicionales" + 
-      "y creación de una pieza propia para llevarse a casa. Duración aproximada de 3h.", "https://images.pexels.com/photos/9736884/pexels-photo-9736884.jpeg"),
-      new Evento("HACER PAN", "Culinario", "20 de mayo de 2024", "Arroyo de la Luz, Cáceres", 10, 18, "Demostración de las técnicas utilizadas" + 
-      "en el pastoreo y cuidado del rebaño. Duración aproximada de 1h.", "https://images.pexels.com/photos/4921212/pexels-photo-4921212.jpeg"),
-      new Evento("VENDIMIA EN LA VIÑA", "Agrícola", "20 de mayo de 2024", "Arroyo de la Luz, Cáceres", 10, 18, "Demostración de las técnicas utilizadas" + 
-      "en el pastoreo y cuidado del rebaño. Duración aproximada de 1h.", "https://images.pexels.com/photos/4921212/pexels-photo-4921212.jpeg")
+      " y creación de una pieza propia para llevarse a casa. Duración aproximada de 3h.", "https://images.pexels.com/photos/9736884/pexels-photo-9736884.jpeg"),
+      new Evento("PASTOR POR UN DÍA", "Ganadería", "20 de mayo de 2024", "Arroyo de la Luz, Cáceres", 10, 18, "Demostración de las técnicas utilizadas en el pastoreo," + 
+      " cuidado del rebaño y paseo por los caminos colindates. Duración aproximada de 1h.", "https://images.pexels.com/photos/4921193/pexels-photo-4921193.jpeg"),
+      new Evento("ELABORACIÓN DE PAN RÚSTICO", "Culinario", "22 de mayo de 2024", "Budia, Guadalajara", 15, 20, "Demostración de las técnicas utilizadas" + 
+      " para la elaboración de repostería casera y elaboración individual de una barra de pan.", "https://images.pexels.com/photos/10009354/pexels-photo-10009354.jpeg"),
+      new Evento("VENDIMIA EN LA VIÑA", "Agrícola", "20 de mayo de 2024", "Medellín, Badajoz", 20, 20, "Visita a viñedos para la recogida de uvas y" + 
+      " cata didáctica y lúdica para entender la importancia de los aromas del vino.",  "https://images.pexels.com/photos/5529569/pexels-photo-5529569.jpeg")
     ]
   }
 
